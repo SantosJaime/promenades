a950c7bfe18eb5eeab4113a03a6da65882a0f7d0
SantosJaime <jaime.santos@hotmail.ch>
2019-07-12 09:59:50 +0200

    Modification index, create promenade et style, creation process-create

--- www/create-promenade.php
+++ www/create-promenade.php
@@ -16,27 +16,73 @@ $database= new Database();
     <meta name="viewport" content="width, initialscale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="style.css">
 
+    <header>
+        <!-- Barre de Nav -->
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark indigo">
+                <img src="Images/80931466-signe-de-démarrage-empreinte-vecteur-icône-blanche-avec-une-ombre-douce-sur-un-fond-transparent-.jpg"
+                width="130" height="130" alt="" <a class="navbar-brand" href="#"><strong>Promenades</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Maison <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="create-promenade.php">Ajouter promenade</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contactez-nous</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Fin Barre de Nav -->
+        <div class="view intro-2">
+            <div class="full-bg-img">
+                <div class="mask rgba-black-light flex-center">
+                    <div class="container text-center white-text">
+                    <div class="white-text text-center wow fadeInUp">
+                        <br>
+                        <h3>Ce n'est pas la destination qui compte, mais le trajet</h3>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
     <body>
-        <div class="formulaire">
-            <h2> Création d'une nouvelle promenade </h2>
-            <form action="process-create.php" method="post">
-                <label for="titre">Titre </label>
-                <input type="text" id="pays" name="pays" placeholder ="Suisse"> 
-                <label for="ville">Ville</label>
-                <input type="text" id="ville" name="ville" placeholder ="Genève">
-                <label for="depart">Départ</label>
-                <input type="text" id="depart" name="depart" placeholder ="lieu de départ">
-                <label for="arrivee">Arrivée</label>
-                <input type="text" id="arrivee" name="arrivee" placeholder ="lieu d'arrivée">
-                <label for="codePostal">Code postal </label>
-                <input type="number" id="codePostal" name="codePostal" placeholder ="1227">
-                <label for="pseudo">Pseudo</label>
-                <input type="text" id="pseudo" name="pseudo" placeholder ="Auteur">
-                <label for="detail">Description </label>
-                <input type="text" id="detail" name="detail" placeholder ="Description de la ballade">
-                <label for="images">Image</label>
-                <input type="file" id="images" name="images" placeholder ="Insérer une image">
-            </form>
-        </div>  
+
+    <div class="container-fluid">
+        <div class="col-sm-6">    
+            <div class="formulaire">
+                <h2> Création d'une nouvelle promenade </h2>
+                <form action="process-create.php" method="post">
+                    <label for="titre">Titre </label>
+                    <input type="text" id="pays" name="pays" placeholder ="Suisse"> 
+                    <label for="ville">Ville</label>
+                    <input type="text" id="ville" name="ville" placeholder ="Genève">
+                    <label for="depart">Départ</label>
+                    <input type="text" id="depart" name="depart" placeholder ="lieu de départ">
+                    <label for="arrivee">Arrivée</label>
+                    <input type="text" id="arrivee" name="arrivee" placeholder ="lieu d'arrivée">
+                    <label for="codePostal">Code postal </label>
+                    <input type="number" id="codePostal" name="codePostal" placeholder ="1227">
+                    <label for="pseudo">Pseudo</label>
+                    <input type="text" id="pseudo" name="pseudo" placeholder ="Auteur">
+                    <label for="detail">Description </label>
+                    <textarea type="text" id="detail" name="detail" rows="3" placeholder ="Description de la ballade" class="form-control md-textarea"></textarea>
+                    <label for="images">Image</label>
+                    <input type="file" id="images" name="images" placeholder ="Insérer une image">
+                
+                </form>
+            </div>
+        </div>
+    </div>  
     </body>
 </html>
---
+++ www/process-create.php
@@ -0,0 +1,27 @@
+<?php
+//PAGE INTERMEDIAIRE => QUE DU PHP
+//import database
+require_once ("Database.php");
+//création connexion
+$database = new Database();
+
+//récupérer infos du formulaire avec $_POST
+
+//appeler la fonction insertDog en passant infos du formulaire
+$insertPromenade = $Database->insertPromenade(
+    $_POST['titre'],
+    $_POST['pays'],
+    $_POST['ville'],
+    $_POST['depart'],
+    $_POST['arrivee'],
+    $_POST['codePostal'],
+    $_POST['pseudo'],
+    $_POST['detail'],
+    $_POST['images']);
+//récupérer le nouvel id de la promenade
+
+
+//Rediriger l'utilisateur vers la page de la promenade crée
+header("Location: detailCircuit.php?id=$insertPromenade");
+
+?>

3878adc7bc066f3f1e17c043f18b3380016c3203
SantosJaime <jaime.santos@hotmail.ch>
2019-07-15 16:00:52 +0200

    Mise à jour et simplification de la NavBar

--- www/index.php
+++ www/index.php
@@ -7,6 +7,7 @@ $database = new Database();
 $listeProm = $database->getPromenades();
 
 ?>
+
 <html>
 
   <head>
@@ -15,62 +16,47 @@ $listeProm = $database->getPromenades();
     <meta name="viewport" content="width, initialscale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Yeseva+One&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Neucha&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
 
-  <header>
       <!-- Barre de Nav -->
       <nav class="navbar fixed-top navbar-expand-lg navbar-dark indigo">
               <img src="Images/stepAndRepeat.png"
-              width="150" height="130" alt="" <a class="navbar-brand" href="#"><strong>Promenades</strong></a>
+              width="150" height="130" alt="" <a class="navbar-brand" href="#"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
               aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
           </button>
+         
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
+              <ul class="navbar-nav">
                   <li class="nav-item active">
-                      <a class="nav-link" href="index.php">Maison <span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="index.php"><strong>Maison </strong><span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="create-promenade.php">Ajouter promenade</a>
+                      <a class="nav-link" href="create-promenade.php"><strong>Ajouter promenade </strong></a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="#">Contactez-nous</a>
+                      <a class="nav-link" href="#"><strong>Contactez-nous </strong></a>
                   </li>
               </ul>
           </div>
       </nav>
       <!-- Fin Barre de Nav -->
-      <div class="view intro-2">
-          <div class="full-bg-img">
-              <div class="mask rgba-black-light flex-center">
-                  <div class="container text-center white-text">
-                      <div class="white-text text-center wow fadeInUp">
-                          <br>
-                          <h3>Ce n'est pas la destination qui compte, mais le trajet</h3>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-    </header>
-
+         
     <body>
 
     <div class="jumbotron text-center">
-      <h1>Nos Circuits </h1>
+      <h3>Nos Circuits </h3>
       <p>Explorez-les ! </p>
     </div>
 
     <div class="container-fluid indexListe">
-      <div class="row">
+      <div class="row align-items-center">
         <?php foreach ($listeProm as $Promenade){ ?>
-       
-          <div class="col-sm-5 ">
+          <div class="col-md-5 ">
             <a href="detailCircuit.php?id=<?php echo $Promenade->getId();?>"><img class="img-fluid" src="Images/prom1.jpg"  alt="Prom1"></a>     
             <h4><?php echo "Promenade " .$Promenade->getTitre();?></h4>
             <?php echo "Auteur : ".$Promenade->getPseudo()." | ".$Promenade->getPays()."/".$Promenade->getVille(); ?>
@@ -79,8 +65,11 @@ $listeProm = $database->getPromenades();
       </div>
     </div>
 
+
     <footer>
+      <br>
     </footer>
+
   </body>
 
 
